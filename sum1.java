4e5dbd0b069ee92c32bb48e98efafbe43ff3262a
Amanpatidar8085 <patidaraman444@gmail.com>
2025-09-03 00:52:41 +0530

    java

---
+++ sum1.java
@@ -0,0 +1,18 @@
+
+    import java.util.Scanner;
+    public class sum1{
+    public static void main(String []args){
+    System.out.println("Enter size of the array");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int []arr=new int[n];
+    System.out.println("enter array element");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        System.out.println(arr[i]+ " ");
+    }
+    }
+    }  
+
