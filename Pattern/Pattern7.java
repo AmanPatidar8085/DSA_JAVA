f96437f54201a43b6ba6d3578d4e4adbaeeccf71
Amanpatidar8085 <patidaraman444@gmail.com>
2025-12-28 11:39:17 +0530

    Triangle

---
+++ Pattern/Pattern7.java
@@ -0,0 +1,23 @@
+package Pattern;
+import java.util.*;
+public class Pattern7{
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a size of row");
+    int n1=sc.nextInt();
+    // System.out.println("enter the size of col");
+    // int n2=sc.nextInt();
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n1-i-1;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<2*i+1;j++){
+            System.out.print("*");
+        }
+        for(int j=0;j<n1-i-1;j++){
+            System.out.print(" ");
+        }
+        System.out.println(" ");
+    }
+   }
+}
