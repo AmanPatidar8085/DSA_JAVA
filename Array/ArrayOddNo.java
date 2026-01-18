7fa53e96c4d8f6324856b68f6c51c33c38af9969
Amanpatidar8085 <patidaraman444@gmail.com>
2026-01-18 17:29:07 +0530

    Array

---
+++ Array/ArrayOddNo.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class ArrayOddNo {
+    public static void main(String[] args) {
+        int[]arr={14,12,17,15,16};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
---
+++ Array/ArrayReverse.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[]arr={14,12,17,15,16};
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0){
+                System.out.println(arr[i]);
+            }
+        }
+}
+}
